4e90cf1f88c6012f624ab34e14f49964c4cbace2
vishaka5892 <vishaka.4167@gmail.com>
2023-11-28 14:15:37 -0800

    Adding API for Menu and Booking commit

---
+++ littlelemon/restaurant/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Menu, Booking
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = ['id', 'title', 'price', 'inventory']
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
+from rest_framework import generics, viewsets
+from rest_framework.decorators import api_view
 
 # Create your views here.
 def index(request):
     return render(request, "index.html", {})
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+    
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    
