9818c11797ffd24acc74012c74b310d7171e13ad
vishaka5892 <vishaka.4167@gmail.com>
2023-11-29 14:23:18 -0800

    Testing the API

---
+++ littlelemon/littlelemon/tests/test-views.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from .models import Menu
+from .serializers import MenuSerializer
+
+
+class MenuViewTest(TestCase):
+    def setup(self):
+        self.menu_item1 = Menu.objects.create(title="Cheesecake", price =11.00, inventory = 10)
+        self.menu_item2 = Menu.objects.create(title="Pizza", price = 8, inventory= 20)
+        
+    def test_getall(self):
+        response = self.client.get('/restaurant/menu/items/')
+        menu_items = Menu.objects.all()
+        serializer = MenuSerializer(menu_items, many=True)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, 200)
--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -6,8 +6,13 @@ class Booking(models.Model):
     no_of_guests = models.IntegerField()
     booking_date = models.DateTimeField()
     
+    def __str__(self) -> str:
+        return f'{self.name} - {self.booking_date}'
     
 class Menu(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField()
+    
+    def __str__(self) -> str:
+        return f'{self.title} : {str(self.price)}'
